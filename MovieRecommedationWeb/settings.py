21b715124a222a6824c0d7a3a3a80d1ca9e4ac6b
巫文杰 <252587809u@qq.com>
2023-07-22 07:47:51 +0800

    实现了用户忘记密码逻辑 #todo 展示用户详细资料

--- MovieRecommedationWeb/settings.py
+++ MovieRecommedationWeb/settings.py
@@ -128,8 +128,14 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-INTERNAL_IPS = [
-    # ...
-    "127.0.0.1",
-    # ...
-]
+# INTERNAL_IPS = [
+#     # ...
+#     "127.0.0.1",
+#     # ...
+# ]
+
+if DEBUG:
+    import socket  # only if you haven't already imported this
+
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
